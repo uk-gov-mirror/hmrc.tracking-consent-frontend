d517b3ae440ec9b92b7e272c74d6de583238af98
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2025-02-17 13:57:13 +0000

    PLATUI-3500 add comments to denote usage of filter

--- app/uk/gov/hmrc/trackingconsentfrontend/filters/CookieConsentFilter.scala
+++ app/uk/gov/hmrc/trackingconsentfrontend/filters/CookieConsentFilter.scala
@@ -21,7 +21,7 @@ import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 class CookieConsentFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
-
+// This filter is not intended to stay forever, but rather to alleviate an issue with a bug, as per PLATUI-3500
   override def apply(next: EssentialAction): EssentialAction = EssentialAction { requestHeader =>
     next(requestHeader).map { result =>
       val modifiedCookies = requestHeader.cookies.map { c =>
